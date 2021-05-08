def8272055f86fd75bc652bf0c6ba48683a6bef2
kosuke_homma <1644675+honmakousuke@users.noreply.github.com>
2021-05-08 16:54:58 +0900

    config edit

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|gif|svg)/,
         use: [
           {
             loader: 'file-loader',
